f68bf78a136e559224fb11436d9c4018a40d2eb6
MacBook Pro <macbookpro@indomacs-MacBook-Pro-5.local>
2023-03-17 15:26:48 +0700

    first init

---
+++ LatihanCoba1/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  LatihanCoba1
+//
+//  Created by MacBook Pro on 17/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var rocketButton: UIButton!
+    @IBOutlet weak var engineerButton: UIButton!
+    @IBOutlet weak var singerButton: UIButton!
+    @IBOutlet weak var astronoutButton: UIButton!
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Helpers
+    func showAlert(title: String, subtitle: String) {
+        let alert = UIAlertController(title: title, message: subtitle, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+        //TODO: Implement alert
+    }
+    
+    // MARK: - Actions
+    @IBAction func astronoutButtonTapped(_ sender: Any) {
+        showAlert(title: "Your job", subtitle: "Astronout")
+    }
+    @IBAction func singerButtonTapped(_ sender: Any) {
+        showAlert(title: "Your job", subtitle: "Singer")
+    }
+    @IBAction func engineerButtonTapped(_ sender: Any) {
+        showAlert(title: "Your job", subtitle: "Engineer")
+    }
+    @IBAction func rocketButtonTapped(_ sender: Any) {
+        showAlert(title: "Your job", subtitle: "Rocket")
+    }
+}
+
---
+++ Tugas Praktek Sesi 1.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+//**Latihan 1:**
+//Ada persamaan Matematika seperti ini:
+//`x = y + 10`
+//
+//Jika `x` ingin bernilai `40`, bagaimana caranya apabila dibuat dalam Swift code?
+//
+//💡 tentukan yang mana yang jadi variable, yang mana yang jadi constant dan ketika `print(x)` hasilnya 40
+
+var y = 30
+var x = y + 10
+print(x)
